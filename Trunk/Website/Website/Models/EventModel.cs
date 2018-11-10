94c7c07702a0accb409bbd4f4e3dd54e410533b1
Chandler <chandlerdgls@gmail.com>
2018-11-10 12:19:40 -0500

    Made the LandingPage dynamic with the events

---
+++ Trunk/Website/Website/Models/EventModel.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class EventModel
+    {
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Month { get; set; }
+
+        public string Description { get; set; }
+
+        public RatingEnum Rating { get; set; }
+    }
+}
---
+++ Trunk/Website/Website/Models/HomeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class HomeModel
+    {
+        public List<EventModel> Events { get; set; }
+    }
+}
